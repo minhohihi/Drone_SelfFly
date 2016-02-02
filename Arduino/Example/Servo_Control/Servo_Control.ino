ea360bd9c519d1f3a9729450c429606bea10976d
MinHo Park <minhohihi@gmail.com>
2016-02-02 23:36:21 +0900

    1. Insert Servo Control Example

---
+++ Arduino/Example/Servo_Control/Servo_Control.ino
@@ -0,0 +1,21 @@
+#include <Servo.h> 
+ 
+Servo servo;
+ 
+int servoPin = 9;
+int angle = 0; // servo position in degrees 
+ 
+void setup() 
+{ 
+  servo.attach(servoPin);
+} 
+ 
+void loop() 
+{ 
+  // rotate from 0 to 180 degrees
+  for(angle = 0; angle < 180; angle++) 
+  { 
+    servo.write(angle); 
+    delay(10); 
+  } 
+}
